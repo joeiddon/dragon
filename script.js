c4f6c14249d33e8d4e32b535cd18827841dfc806
joeiddon <joedominiciddon@gmail.com>
2020-05-13 17:39:56 +0100

    load event listener

--- script.js
+++ script.js
@@ -61,9 +61,11 @@ gl.bindTexture(gl.TEXTURE_2D, texture);
 gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
               new Uint8Array([0, 0, 255, 255]));
 let image = document.getElementById('texture-img');//new Image();
-gl.bindTexture(gl.TEXTURE_2D, texture);
-gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-gl.generateMipmap(gl.TEXTURE_2D);
+image.addEventListener('load', function() {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.generateMipmap(gl.TEXTURE_2D);
+});
 
 let textures = {
     // format of values: [min corner, max corner] - from top left of image
